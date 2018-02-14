0bf60caa34b6e9875407110254694166be898aa3
mortheria <cakir-enes@outlook.com>
2018-02-14 21:19:52 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,26 +1,71 @@
 package com.mycompany.app;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+public class App {
 
 	public static boolean search(ArrayList<Integer> array, int e) {
 		System.out.println("inside search");
-		if (array == null) return false;
-	
-		for (int elt : array) {
-			if (elt == e) return true;
-	    }
-		return false;
-		
-		
-	}
-}
+		if (array == null)
+			return false;
 
+		for (int elt : array) {
+			if (elt == e)
+				return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		port(getHerokuAssignedPort());
+
+		get("/", (req, res) -> "Hello, World");
+
+		post("/compute", (req, res) -> {
+			String in1 = req.queryParams("input1");
+			java.util.Scanner sc1 = new Scanner(in1);
+			sc1.useDelimiter("[;\r\n]+");
+			ArrayList<Integer> inList = new ArrayList<>();
+			while (sc1.hasNext()) {
+				int val = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
+				inList.add(val);
+			}
+			System.out.println(inList);
+
+			String in2 = req.queryParams("input2").replaceAll("\\s", "");
+			int in2AsInt = Integer.parseInt(in2);
+
+			boolean result = App.search(inList, in2AsInt);
+
+			Map map = new HashMap<>();
+			map.put("result", result);
+			return new ModelAndView(map, "compute.mustache");
+		}, new MustacheTemplateEngine());
+
+		get("/compute", (rq, rs) -> {
+			Map map = new HashMap();
+			map.put("result", "not computed yet!");
+			return new ModelAndView(map, "compute.mustache");
+		}, new MustacheTemplateEngine());
+	}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567;
+	}
+
+}
